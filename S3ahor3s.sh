5ce1f289907938ad0c733b7dc33e678b2c83f257
jz-shz <jzsilverhawk@gmail.com>
2024-10-06 20:32:13 -0700

    Create S3ahor3s.sh

---
+++ S3ahor3s.sh
@@ -0,0 +1,17 @@
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run this script as root. You can do so by using 'sudo su'."
+    exit
+
+echo "+##############################################+"
+echo "| Welcome to S3ahors3!                         |"
+echo "| The User Extension Disabler                  |"
+echo "| -------------------------------------------- |"
+echo "| Developers:                                  |"
+echo "| - jz                                         |"
+echo "+##############################################+"
+
+sleep 1
+
+if [ $(
+
+rm -r /home/user/chronos/Extensions/haldlgldplgnggkjaafhelgiaglafanh
