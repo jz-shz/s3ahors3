f8c7ac908bac739305c9601cda0727646b3ae6e5
jz-shz <jzsilverhawk@gmail.com>
2024-10-06 20:34:48 -0700

    Create Pollen.sh

---
+++ Payloads/Pollen.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run this script as root. You can do so by using 'sudo su'."
+    exit
+fi
+
+echo "+##############################################+"
+echo "| Welcome to Pollen!                           |"
+echo "| The User Policy Editor                       |"
+echo "| -------------------------------------------- |"
+echo "| Developers:                                  |"
+echo "| - OlyB                                       |"
+echo "| - Rafflesia                                  |"
+echo "| - r58Playz                                   |"
+echo "+##############################################+"
+echo "May Ultrablue rest in peace, o7."
+
+
+sleep 1
+
+mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
+echo '{
+  "URLBlocklist": [],
+  "SystemFeaturesDisableList": [],
+  "EditBookmarksEnabled": true,
+  "ChromeOsMultiProfileUserBehavior": "unrestricted",
+  "DeveloperToolsAvailability": 1,
+  "DefaultPopupsSetting": 1,
+  "AllowDeletingBrowserHistory": true,
+  "AllowDinosaurEasterEgg": true,
+  "IncognitoModeAvailability": 0,
+  "AllowScreenLock": true,
+  "ExtensionAllowedTypes": null,
+  "ExtensionInstallAllowlist": null,
+  "ExtensionInstallBlocklist": null,
+  "ExtensionInstallForcelist": null,
+  "ExtensionSettings": null,
+  "PasswordManagerEnabled": "true",
+  "TaskManagerEndProcessEnabled": "true",
+  "UptimeLimit": "null",
+  "SystemTerminalSshAllowed": "true",
+  "SystemTimezone": "",
+  "IsolatedAppsDeveloperModeAllowed": "true",
+  "ForceGoogleSafeSearch": "false",
+  "ForceYouTubeRestrict": "0",
+  "EasyUnlockAllowed": "true",
+  "DisableSafeBrowsingProceedAnyway": "false",
+  "DeviceAllowNewUsers": "true",
+  "DevicePowerAdaptiveChargingEnabled": "true",
+  "DeviceGuestModeEnabled": "true",
+  "DeviceUnaffiliatedCrostiniAllowed": "true",
+  "VirtualMachinesAllowed": "true",
+  "CrostiniAllowed": "true",
+  "DefaultCookiesSetting": "1",
+  "VmManagementCliAllowed": "true",
+  "WifiSyncAndroidAllowed": "true",
+  "DeveloperToolsDisabled": "false",
+  "DeveloperToolsAvailability": "1",
+  "DeviceBlockDevmode": "false",
+  "UserBorealisAllowed": "true",
+  "InstantTetheringAllowed": "true",
+  "NearbyShareAllowed": "true",
+  "PinnedLauncherApps": "null",
+  "PrintingEnabled": "true",
+  "SmartLockSigninAllowed": "true",
+  "PhoneHubAllowed": "true",
+  "LacrosAvailability": "user_choice",
+  "WallpaperImage": null,
+  "ArcPolicy": {
+    "playStoreMode": "ENABLED",
+    "installType": "FORCE_INSTALLED",
+    "playEmmApiInstallDisabled": false,
+    "dpsInteractionsDisabled": false
+  },
+  "DnsOverHttpsMode": "automatic",
+  "BrowserLabsEnabled": "true",
+  "ChromeOsReleaseChannelDelegated": "true",
+  "WallpaperImage": "null",
+  "SafeSitesFilterBehavior": "0",
+  "SafeBrowsingProtectionLevel": "0",
+  "DownloadRestrictions": "0",
+  "ProxyMode": "system",
+  "ProxyServerMode": "system",
+  "NetworkThrottlingEnabled": "false",
+  "NetworkPredictionOptions": "0",
+  "AllowedDomainsForApps": "",
+  "DeviceUserAllowlist": ""
+}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
+cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
+mount --bind /tmp/overlay/etc /etc
+
+echo ""
+echo "Pollen has been successfully applied!"
